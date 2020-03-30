e6f0d0b45c77547695cb12c08d174dd82caa251c
sowngl123 <sowngl123@gmail.com>
2020-03-31 01:33:50 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { BuildingComponent } from './building/building.component';
-import { ContactComponent } from './contact/contact.component';
-import { ManagerComponent } from './manager/manager.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { ProductAddComponent } from './product-add/product-add.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductEditComponent } from './product-edit/product-edit.component';
-import { ProuctDeleteComponent } from './prouct-delete/prouct-delete.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { BuildingComponent } from "./building/building.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ManagerComponent } from "./manager/manager.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { ProductAddComponent } from "./product-add/product-add.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { ProductEditComponent } from "./product-edit/product-edit.component";
+import { ProuctDeleteComponent } from "./prouct-delete/prouct-delete.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, BuildingComponent, ContactComponent, ManagerComponent, NotFoundComponent, ProductAddComponent, ProductDetailComponent, ProductEditComponent, ProuctDeleteComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HomeComponent,
+    AboutComponent,
+    BuildingComponent,
+    ContactComponent,
+    ManagerComponent,
+    NotFoundComponent,
+    ProductAddComponent,
+    ProductDetailComponent,
+    ProductEditComponent,
+    ProuctDeleteComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
