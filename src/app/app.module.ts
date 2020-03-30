a823a8ebbe3696b2f2a1c68a50ee64a885a38fc4
sowngl123 <sowngl123@gmail.com>
2020-03-31 01:27:13 +0700

    2 files modified, 13 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,16 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { BuildingComponent } from './building/building.component';
 import { ContactComponent } from './contact/contact.component';
+import { ManagerComponent } from './manager/manager.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProuctDeleteComponent } from './prouct-delete/prouct-delete.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, BuildingComponent, ContactComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, BuildingComponent, ContactComponent, ManagerComponent, NotFoundComponent, ProductAddComponent, ProductDetailComponent, ProductEditComponent, ProuctDeleteComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/prouct-delete/prouct-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prouct-delete',
+  templateUrl: './prouct-delete.component.html',
+  styleUrls: ['./prouct-delete.component.css']
+})
+export class ProuctDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
