71b2c36121e865c260ea5ed2a70ddba5db029afd
sowngl123 <sowngl123@gmail.com>
2020-03-31 05:30:53 +0700

    1 file modified

--- src/app/product-add/product-add.component.ts
+++ src/app/product-add/product-add.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from '../Product';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent implements OnInit {
-
-  constructor() { }
+  product: Product = new Product();
+  constructor(
+    private productService: ProductService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
-
+  addProduct(){
+    this.productService.addProduct(this.product).subscribe(data => this.router.navigateByUrl('/list-product'));
+  }
 }
